5defa5fe245ae6db601053be899e545e49122eb3
papasgeorge1808 <papasgeorge1808@gmail.com>
2024-07-21 22:35:41 +0300

    Fix class user stucture

--- firstbootproject/src/main/java/com/johndoe83/firstbootproject/model/User.java
+++ firstbootproject/src/main/java/com/johndoe83/firstbootproject/model/User.java
@@ -47,14 +47,10 @@ public class User {
         this.age = age;
     }
 
+
     @Override
     public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", age=" + age +
-                '}';
+        return "User{" + "id=" + id + ", name='" + name + '\'' + ", lastName='" + lastName + '\'' + ", age=" + age + '}';
     }
 
     public Long getId() {
