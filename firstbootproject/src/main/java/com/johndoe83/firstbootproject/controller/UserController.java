0e1f5f734a1f5e3215ab0342f60a3c5a804ecbe6
papasgeorge1808 <papasgeorge1808@gmail.com>
2024-07-24 23:51:46 +0300

    Fix requestparam and POST method

--- firstbootproject/src/main/java/com/johndoe83/firstbootproject/controller/UserController.java
+++ firstbootproject/src/main/java/com/johndoe83/firstbootproject/controller/UserController.java
@@ -6,13 +6,11 @@ import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -31,8 +29,8 @@ public class UserController {
         return "users/index";
     }
 
-    @GetMapping("/{id}")
-    public String show(@PathVariable("id") Long id, Model model) {
+    @GetMapping("/show")
+    public String show(@RequestParam("id") Long id, Model model) {
         model.addAttribute("user", userService.show(id));
         return "users/show";
     }
@@ -42,32 +40,28 @@ public class UserController {
         return "users/new";
     }
 
-    @PostMapping()
+    @PostMapping("/new")
     public String create(@ModelAttribute("user") @Valid User user, BindingResult bindingResult) {
-        if (bindingResult.hasErrors())
-            return "users/new";
+        if (bindingResult.hasErrors()) return "users/new";
         userService.save(user);
         return "redirect:/users";
     }
 
-    @GetMapping("/{id}/edit")
-    public String edit(Model model, @PathVariable("id") Long id) {
+    @GetMapping("/edit")
+    public String edit(Model model, @RequestParam("id") Long id) {
         model.addAttribute("user", userService.show(id));
         return "users/edit";
     }
 
-    @PatchMapping("/{id}")
-    public String update(@ModelAttribute("user") @Valid User user,
-                         BindingResult bindingResult,
-                         @PathVariable("id") Long id) {
-        if (bindingResult.hasErrors())
-            return "users/edit";
+    @PostMapping("/edit")
+    public String update(@ModelAttribute("user") @Valid User user, BindingResult bindingResult, @RequestParam("id") Long id) {
+        if (bindingResult.hasErrors()) return "users/edit";
         userService.update(id, user);
         return "redirect:/users";
     }
 
-    @DeleteMapping("/{id}")
-    public String delete(@PathVariable("id") Long id) {
+    @PostMapping("/delete")
+    public String delete(@RequestParam("id") Long id) {
         userService.delete(id);
         return "redirect:/users";
     }
